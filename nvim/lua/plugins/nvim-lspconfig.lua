44669ad76406d9ddb842769875ccd00030a7c650
Maximilian Seidler <maximilian.seidler@soundwork.at>
2022-08-20 13:15:47 +0200

    Revert "added vscode langservers"
    This reverts commit 982c3297c70fff50512a464985b2a9467aa48cd7.

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -128,17 +128,7 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches.
 -- Add your language server below:
-local servers = {
-  'bashls',
-  'pyright',
-  'clangd',
-  'html',
-  'cssls',
-  'tsserver',
-  'vscode-css-languageserver',
-  'vscode-json-languageserver',
-  'vscode-html-languageserver'
-}
+local servers = { 'bashls', 'pyright', 'clangd', 'html', 'cssls', 'tsserver' }
 
 -- Call setup
 for _, lsp in ipairs(servers) do
