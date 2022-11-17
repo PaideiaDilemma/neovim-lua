bc0d5fae785509fb552e739bf2fc0308c0d4b9ff
Maximilian Seidler <maximilian.seidler@student.tugraz.at>
2022-11-17 13:25:26 +0100

    added nvim-telescope

--- nvim/lua/packer_init.lua
+++ nvim/lua/packer_init.lua
@@ -119,6 +119,12 @@ return packer.startup(function(use)
   }
   vim.g.mkdp_auto_start = 1
 
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+  -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
