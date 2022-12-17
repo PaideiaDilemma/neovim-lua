b1f886e312db12f1d688c3679816df629b4bb7fc
Maximilian Seidler <maximilian.seidler@student.tugraz.at>
2022-12-17 11:41:25 +0100

    relative line numbers

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -21,6 +21,7 @@ opt.completeopt = 'menuone,noinsert,noselect'  -- Autocomplete options
 -- Neovim UI
 -----------------------------------------------------------
 opt.number = true           -- Show line number
+opt.relativenumber = true   -- Relative line numbers
 opt.showmatch = true        -- Highlight matching parenthesis
 opt.foldmethod = 'marker'   -- Enable folding (default 'foldmarker')
 opt.colorcolumn = '80'      -- Line lenght marker at 80 columns
@@ -30,7 +31,8 @@ opt.ignorecase = true       -- Ignore case letters when search
 opt.smartcase = true        -- Ignore lowercase for the whole pattern
 opt.linebreak = true        -- Wrap on word boundary
 opt.termguicolors = true    -- Enable 24-bit RGB colors
-opt.laststatus=3            -- Set global statusline
+opt.laststatus = 3          -- Set global statusline
+opt.scrolloff = 10          -- Scroll, once 10 lines are between cursor and top/bottom
 
 -----------------------------------------------------------
 -- Tabs, indent
@@ -48,6 +50,7 @@ opt.history = 100           -- Remember N lines in history
 opt.lazyredraw = true       -- Faster scrolling
 opt.synmaxcol = 240         -- Max column for syntax highlight
 opt.updatetime = 250        -- ms to wait for trigger an event
+opt.undofile = true         -- use an undo file
 
 -----------------------------------------------------------
 -- Startup
